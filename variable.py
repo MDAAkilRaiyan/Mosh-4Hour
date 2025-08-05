dc70c883a259bfb8757b2e932a66981066f6a2a7
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-05 23:45:11 +0600

    Tried Different Data Types

--- variable.py
+++ variable.py
@@ -1,3 +1,18 @@
 # Variables
+
+# Integer Numbers
 price = 10
+price = 20
 print(price)
+
+# Floating Numbers
+rating = 7.9
+print(rating)
+
+# String
+name = "Md. Aakil Raiyan"
+print(name)
+
+# Boolean
+ready = True
+print(ready)
