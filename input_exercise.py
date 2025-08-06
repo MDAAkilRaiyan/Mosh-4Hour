141bffd3bed756a025b07c08a24f7829b6e145f2
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-06 23:21:37 +0600

    Input Exercise Done

---
+++ input_exercise.py
@@ -0,0 +1,6 @@
+# Input Taking and Output Show Exercise
+
+name = input("What is your name, sir? ")
+fav_color = input("And what color do you prefer? ")
+
+print(name + " likes " + fav_color + " color")
