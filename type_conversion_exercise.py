7640d9fb1181d9b70c2391366346d1655b5bfa08
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-07 23:48:17 +0600

    Weight conversion exercise done

---
+++ type_conversion_exercise.py
@@ -0,0 +1,7 @@
+# Type Conversion Exercise
+
+pound = input("What is your weight in Pounds? ")
+
+kilogram = float(pound) / 2.205
+
+print("So, in Kilogram, your weight will be " + str(kilogram) + "kg")
