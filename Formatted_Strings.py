02bc34f03a2c255eef334744524d5e8df8263bc4
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-10 21:42:54 +0600

    Formatted Strings Secured

---
+++ Formatted_Strings.py
@@ -0,0 +1,11 @@
+# Formatted Strings
+line1 = "John"
+line2 = "Cena"
+
+# Regular Style
+message = "You can't see " + line1 + " " + line2
+print(message)
+
+# Formatted Style
+msg = f"You can't see {line1} {line2}"
+print(msg)
