8e5c1281e5c34f886644a87181e56351c6898ca8
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-11 12:29:26 +0600

    Lower Case added

--- String_Methods.py
+++ String_Methods.py
@@ -4,5 +4,10 @@ course = "Python for Noobs"
 # Finding Length
 print(len(course))
 
-# Making Upper Case
+# Making Upper Case (It Copies the Original and Does not change the Original Variable)
 print(course.upper())
+print(course)
+
+# Making Lower Case (It Copies the Original and Does not change the Original Variable)
+print(course.lower())
+print(course)
