79588a10199c7d98cff01dd685f591cea8a5a2b3
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-15 11:46:16 +0600

    Learnt the new Augmented Operations

---
+++ Augmented_Operations.py
@@ -0,0 +1,16 @@
+# Augmented Assignment Operations
+
+# Usual Long Form
+x = 15
+x = x + 5
+print(x)
+
+# Augmented Short Forms
+x += 5  # For Increment
+print(x)
+x -= 5  # For Decrement
+print(x)
+x *= 5  # For Multiplication
+print(x)
+x /= 5  # For Division
+print(x)
