ab756da0b9357590e4f62c45fcfd4a3a91f8578b
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-15 22:15:44 +0600

    Imported math module and used Ceil and Flood methods

--- Math_Functions.py
+++ Math_Functions.py
@@ -1,3 +1,6 @@
+import math
+# We can use many math functions by importing the math module and use it by math.
+
 # Functions to Help in Math Operations
 
 # Round function to Round Numbers
@@ -8,3 +11,13 @@ print(round(x))
 # Absolute function to make any number positive
 x = -345
 print(abs(x))
+
+# Ceil Method from the math module
+# This method rounds a number up to the smallest integer greater than or equal to x
+x = 8.394
+print(math.ceil(x))
+
+# Floor Method from the math module
+# This method rounds a number down to the largest integer less than or equal to x
+x = 2.234
+print(math.floor(x))
