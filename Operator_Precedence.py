8ab8097fa06ac81e9999b75787bb763419775dd3
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-15 11:55:42 +0600

    Learned the Precedence of Operations

---
+++ Operator_Precedence.py
@@ -0,0 +1,13 @@
+# Operator Precedence
+
+# The Order of Precedence
+# ->Exponentiation aka Power 2**3
+# ->Multiplication or Division * /
+# ->Addition or Subtraction + -
+
+y = 12 ** 2 + 3 * 2
+print(y)
+
+# ->Parenthesis or Brackets always takes priorities
+x = 45 - 15 * (12 * 5) + 200 / 5
+print(x)
