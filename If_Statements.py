0815101d2e77a1d3429cbae771994d4031c16839
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-16 23:23:08 +0600

    'Else' learned

--- If_Statements.py
+++ If_Statements.py
@@ -1,6 +1,11 @@
 # If Statements
-is_hot = True
+is_hot = False
 
 if is_hot:
     print("It's a hot day")
+    print("Drink Plenty of Water")
+else:
+    print("It's a Cold Day")
+    print("Wear warm clothes")
+
 print("Enjoy the day")
