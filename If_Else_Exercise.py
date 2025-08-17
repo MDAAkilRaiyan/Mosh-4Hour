fc35626b5affd8c5131f731fa16ab8ef57b29735
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-17 23:42:34 +0600

    Solved the If Else exercise with Numerical numbers

--- If_Else_Exercise.py
+++ If_Else_Exercise.py
@@ -1,8 +1,20 @@
 # If Else Statement Test
 
+# Without the numerical
 has_good_credit = True
 
 if has_good_credit:
     print("Down Payment is 10%")
 else:
     print("Down Payment is 20%")
+
+# With Numerical
+
+price = 1000000
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+
+print(f"Down payment: ${down_payment}")
