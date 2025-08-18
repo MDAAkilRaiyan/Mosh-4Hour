57e7123ea596872d1666fdbda502775217002dfe
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-18 22:26:53 +0600

    OR Condition done

--- Logical_Operators.py
+++ Logical_Operators.py
@@ -3,9 +3,20 @@
 has_high_income = True
 has_good_credit = False
 
-
+# AND Operation where both conditions must be True
 if has_high_income and has_good_credit:
     print("Eligible for Loan")
 
 else:
     print("Not Eligible for Loan")
+
+
+can_fight_crime = True
+wears_a_cape = False
+
+# OR Operation where at least on condition must be True
+if can_fight_crime or wears_a_cape:
+    print("Is a Superhero")
+
+else:
+    print("Not a Superhero")
