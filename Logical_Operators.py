8efbe0da6aa1aa8aa5cd1733f69f32ada0312c20
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-18 22:22:50 +0600

    AND Operation completed

---
+++ Logical_Operators.py
@@ -0,0 +1,11 @@
+# Logical Operators
+
+has_high_income = True
+has_good_credit = False
+
+
+if has_high_income and has_good_credit:
+    print("Eligible for Loan")
+
+else:
+    print("Not Eligible for Loan")
