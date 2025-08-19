05a2c733ec5ed8b249846c9617a44c77eebaa152
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-20 00:11:10 +0600

    Successfully converted KG to lbs and lbs to kg

--- Weight_Conversion.py
+++ Weight_Conversion.py
@@ -1,5 +1,14 @@
 # Weight Conversion Program
 
-weight = float(input("Weight: "))
+weight = int(input("Weight: "))
 
 unit = input("(L)bs or (K)g: ")
+
+if unit == 'L' or unit == 'l':
+    weight = weight * 0.4536
+    print(f'You are {weight} Kilograms')
+elif unit == 'K' or unit == 'k':
+    weight = weight * 2.205
+    print(f"You are {weight} Pounds")
+else:
+    print("Wrong Unit Entered")
