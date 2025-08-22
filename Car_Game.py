1c4ea3e322b830dc63200eb867cc1a773af8209a
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-22 23:57:04 +0600

    Car Game First Draft Complete

--- Car_Game.py
+++ Car_Game.py
@@ -2,4 +2,19 @@
 ready = input(">")
 
 if ready.upper() == "HELP":
-    print("Working")
+    print("Start - to start the car")
+    print("Stop - to stop the car")
+    print("Quit - to exit")
+    i = 0
+    while (i < 1):
+        response = input("> ")
+        if response.upper() == "START":
+            print("Car Started...Ready to go!")
+        elif response.upper() == "STOP":
+            print("Car Stopped.")
+        elif response.upper() == "QUIT":
+            break
+        else:
+            print("I don't understand that...")
+else:
+    print("Wrong Input")
