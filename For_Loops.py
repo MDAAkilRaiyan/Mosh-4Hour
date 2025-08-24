16a224f7a9386b19e7c898a47d3a89470da11a48
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-25 00:15:30 +0600

    Used range function to print numbers of a certain range

--- For_Loops.py
+++ For_Loops.py
@@ -8,3 +8,7 @@ for item in "Python":
 # Using on a List
 for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
     print(i)
+
+# Using Range Function for creating a range of numbers
+for r in range(200):
+    print(r)
