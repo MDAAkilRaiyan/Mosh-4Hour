2dbc81c289ee241f598e5889b1eb034a01860829
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-26 00:36:16 +0600

    Solved it using Nested Loops

--- Nested_Loops_Challenge.py
+++ Nested_Loops_Challenge.py
@@ -1,6 +1,15 @@
 # Nested Loops Challenge
 
+# Using Simple Arithmetic Operators
 numbers = [5, 2, 5, 2, 2]
 
-for item in numbers:
-    print('x' * item)
+# for item in numbers:
+# print('x' * item)
+
+# Using Nested Loops
+
+for x in numbers:
+    stars = ""
+    for y in range(x):
+        stars += 'x'
+    print(stars)
