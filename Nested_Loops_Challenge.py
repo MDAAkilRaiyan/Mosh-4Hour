a95ef3b88479234be04dbd4e49dce694d1a9115d
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-26 23:52:36 +0600

    Modified the code for an L shape design

--- Nested_Loops_Challenge.py
+++ Nested_Loops_Challenge.py
@@ -8,8 +8,21 @@ numbers = [5, 2, 5, 2, 2]
 
 # Using Nested Loops
 
+print("Below is an 'F' Shape")
 for x in numbers:
     stars = ""
     for y in range(x):
         stars += 'x'
     print(stars)
+
+
+# To Create an L shape
+
+lengths = [1, 1, 1, 1, 5]
+print("Below is an 'L' Shape")
+
+for p in lengths:
+    asterics = ''
+    for r in range(p):
+        asterics += 'x'
+    print(asterics)
