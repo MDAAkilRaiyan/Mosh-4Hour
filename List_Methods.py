ef6966234a81df6be592932f9fe949445dcf6589
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-30 23:53:50 +0600

    Learnt about the Append List Method to add new items to the list

---
+++ List_Methods.py
@@ -0,0 +1,12 @@
+# List Methods
+
+numbers = [10, 45, 76, 31, 69]
+print(f'Original List = {numbers}')
+# Method to Add New Items
+numbers.append(23)
+print(f'Appended List = {numbers}')
+
+
+# Method to Remove Items
+
+# Method to Check if an Item is in the list
