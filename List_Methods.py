a2b57406f7b2034db1758f21c295e03e1d7d6222
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-31 00:00:02 +0600

    Learnt about the Insert Method of List

--- List_Methods.py
+++ List_Methods.py
@@ -2,10 +2,16 @@
 
 numbers = [10, 45, 76, 31, 69]
 print(f'Original List = {numbers}')
+
 # Method to Add New Items
+
+# Adding at the very End
 numbers.append(23)
 print(f'Appended List = {numbers}')
 
+# Adding at a certain position
+numbers.insert(0, 37)
+print(f'Inserted List = {numbers}')
 
 # Method to Remove Items
 
