2df0e1795d7f25f4009e547ae0b8a7a6f045a3ca
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-09-03 23:46:17 +0600

    Solved the List Method Exercise using Mosh's technique

--- List_Method_Exercise.py
+++ List_Method_Exercise.py
@@ -1,11 +1,24 @@
 # List Method Exercise
 
+# This is My Solution
 list = [22, 25, 78, 78, 45, 22, 36, 5, 6, 45, 22]
 
 for li in list:
     occur = list.count(li)
-    for x in range(1, occur):
-        list.remove(li)
+    if occur > 1:
+        for x in range(1, occur):
+            list.remove(li)
+
 
 list.sort()
 print(list)
+
+
+# This is Mosh's Solution
+duplicates = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+originals = []
+for duplicate in duplicates:
+    if duplicate not in originals:
+        originals.append(duplicate)
+
+print(originals)
