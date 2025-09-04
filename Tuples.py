dae08d1ae786b435c508a7ef4610a86f7b163f7c
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-09-04 23:43:25 +0600

    Learnt about Tuples and also the two methods of Tuples

--- Tuples.py
+++ Tuples.py
@@ -1,1 +1,9 @@
-# Tuples
+# Tuples = A list that cannot be modified and defined by ()
+
+numbers = (1, 2, 3)
+# Count is used to count the occurrence of an item
+numbers.count(2)
+print(numbers.count(2))
+# Index to find the index of the first occurrence of an item
+print(numbers.index(1))
+numbers.index(1)
