6993b7a3180be99b0f8107dde4ee621057c0ff6f
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-09-06 23:54:21 +0600

    Learned Python unpacking along with the normal unpacking style

--- Unpacking.py
+++ Unpacking.py
@@ -1,6 +1,13 @@
 # Unpacking
 
 coordinates = (1, 2, 3)
+# Normal Style
 x = coordinates[0]
 y = coordinates[1]
 z = coordinates[2]
+print(f"{x} {y} {z}")
+
+# Unpacking Style
+m, n, o = coordinates
+
+print(f"{m} {n} {o}")
