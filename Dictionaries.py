64746fcf6041d567865460a90b8d60ff047826ae
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-09-08 00:09:59 +0600

    Learnt how to access each key in a dictionary

--- Dictionaries.py
+++ Dictionaries.py
@@ -9,3 +9,10 @@ customer = {
     "age": 50,
     "is_Verified": True
 }
+# Each variable in a Dictionary is called a Key and each key must be Unique
+
+print(customer)
+
+# To access each key, we need to use [] brackets
+
+print(customer["name"])
