d9746802d01f5e2518604e78b8ebe495703e8944
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-09-07 23:45:42 +0600

    Learned how to define a dictionary in Python

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,1 +1,11 @@
-# Dictionaries
+# Dictionaries - to store a bunch of key value pairs
+# Name: John Cena
+# Email: youcantseeme@yahoo.com
+# Phone: 0123456789
+
+# Defining Dictionaries
+customer = {
+    "name": "John Cena",
+    "age": 50,
+    "is_Verified": True
+}
